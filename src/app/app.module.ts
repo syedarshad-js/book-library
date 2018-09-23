7787e2726e565f8af57fe9a669f7758c15b1bbd4
Syed Arshad <syedarshad.js@gmail.com>
2018-09-23 11:30:53 +0530

    Removed @angular/flex-layout and the old seach-box.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-import { SearchBoxComponent } from './search-box/search-box.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { UiModule } from './ui/ui.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchBoxComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +20,6 @@ import { UiModule } from './ui/ui.module';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    FlexLayoutModule,
     UiModule
   ],
   providers: [],
