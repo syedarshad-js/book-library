51bafc90b5b086076adaf3ca210a10d6b7c26a78
Syed Arshad <syedarshad.js@gmail.com>
2018-09-26 02:04:03 +0530

    Updated modal to accommodate issuance and history over ngb-tabset

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+
+const readers = ['Syed Arshad', 'Sam', 'Ran Vijay', 'CNB'];
 
 @Component({
   selector: 'app-modal',
@@ -7,12 +11,26 @@ import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  public issueTo: any;
 
   constructor(
      public activeModal: NgbActiveModal
   ) { }
 
   ngOnInit() {
+
   }
+
+  search = (text$: Observable<string>) =>
+   text$.pipe(
+     debounceTime(200),
+     distinctUntilChanged(),
+     map(term => term.length < 2 ? []
+       : readers.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+   )
+
+   issueBooks(){
+     console.log("issue it")
+   }
 
 }
